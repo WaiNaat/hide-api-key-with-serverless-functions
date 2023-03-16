b5d02c3611617854cfbba90e92b0e5c5c1eb0a1d
Q Kim <qoli0703@gmail.com>
2023-03-16 18:39:13 +0900

    tmdb origin 수정

--- functions/tmdb.js
+++ functions/tmdb.js
@@ -2,7 +2,7 @@ const fetch = require("node-fetch");
 const querystring = require("querystring");
 const stringify = require("../utils/stringify.js");
 
-const TMDB_ORIGIN = "https://api.themoviedb.org";
+const TMDB_ORIGIN = "https://api.themoviedb.org/3/";
 const headers = {
   "Access-Control-Allow-Origin": process.env.HOST,
   "Content-Type": "application/json; charset=utf-8",
